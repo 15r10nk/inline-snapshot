cd68f0f30bd60b5a5126aed4b5f54929640bffb4
Alex Hall <alex.mojaki@gmail.com>
2024-02-25 21:09:44 +0200

    Revert "Construct Snapshot from Source and node, not Executing object"
    This reverts commit 75294bb95f988227617a9b0c6e1b71388b0a10f2.

--- inline_snapshot/_inline_snapshot.py
+++ inline_snapshot/_inline_snapshot.py
@@ -172,7 +172,7 @@ class EqValue(GenericValue):
         if self._new_value is undefined:
             self._new_value = other
 
-        if self._needs_fix() and self._snapshot._node is not None:
+        if self._needs_fix() and self._snapshot._expr.node is not None:
             frame = inspect.currentframe().f_back.f_back
 
             def value_to_node(value):
@@ -226,7 +226,7 @@ class EqValue(GenericValue):
                 return value_to_node(left)
 
             self._new_node = match(
-                self._new_value, self._old_value, self._snapshot._node.args[0]
+                self._new_value, self._old_value, self._snapshot._expr.node.args[0]
             )
 
         return self._visible_value() == other
@@ -491,11 +491,11 @@ def snapshot(obj=undefined):
         node = expr.node
         if node is None:
             # we can run without knowing of the calling expression but we will not be able to fix code
-            snapshots[key] = Snapshot(obj, None, None)
+            snapshots[key] = Snapshot(obj, None)
         else:
             assert isinstance(node.func, ast.Name)
             assert node.func.id == "snapshot"
-            snapshots[key] = Snapshot(obj, expr.source, expr.node)
+            snapshots[key] = Snapshot(obj, expr)
         found_snapshots.append(snapshots[key])
 
     return snapshots[key]._value
@@ -583,15 +583,14 @@ def used_externals(tree):
 
 
 class Snapshot:
-    def __init__(self, value, source, node):
+    def __init__(self, value, expr):
+        self._expr = expr
         self._value = UndecidedValue(value, self)
         self._uses_externals = []
-        self._source = source
-        self._node = node
 
     @property
     def _filename(self):
-        return self._source.filename
+        return self._expr.source.filename
 
     def _format(self, text):
         return format_code(text, Path(self._filename))
@@ -623,11 +622,11 @@ class Snapshot:
         ]
 
     def _change(self):
-        assert self._node is not None
+        assert self._expr is not None
 
         change = ChangeRecorder.current.new_change()
 
-        tokens = list(self._source.asttokens().get_tokens(self._node))
+        tokens = list(self._expr.source.asttokens().get_tokens(self._expr.node))
         assert tokens[0].string == "snapshot"
         assert tokens[1].string == "("
         assert tokens[-1].string == ")"
@@ -672,12 +671,12 @@ class Snapshot:
             )
 
     def _current_tokens(self):
-        if not self._node.args:
+        if not self._expr.node.args:
             return []
 
         return [
             simple_token(t.type, t.string)
-            for t in self._source.asttokens().get_tokens(self._node.args[0])
+            for t in self._expr.source.asttokens().get_tokens(self._expr.node.args[0])
             if t.type not in ignore_tokens
         ]
 
@@ -711,7 +710,7 @@ class Snapshot:
             yield (token.STRING, repr(current_string))
 
     def _needs_update(self):
-        return self._node is not None and [] != list(
+        return self._expr is not None and [] != list(
             self._normalize_strings(self._current_tokens())
         ) != list(self._normalize_strings(self._value_to_token(self._value._old_value)))
 
