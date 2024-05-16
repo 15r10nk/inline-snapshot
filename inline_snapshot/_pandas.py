067456ef1ae80618ff5621edbc22832cf7bc1128
Frank Hoffmann <15r10nk-git@polarbit.de>
2024-05-16 16:16:32 +0200

    feat: implemented all assert_*_equal methods form pandas.testing

--- inline_snapshot/_pandas.py
+++ inline_snapshot/_pandas.py
@@ -2,36 +2,54 @@ from functools import wraps
 from typing import Optional
 
 from pandas import DataFrame
+from pandas import Index
+from pandas import Series
 from pandas.testing import assert_frame_equal as real_assert_frame_equal
+from pandas.testing import assert_index_equal as real_assert_index_equal
+from pandas.testing import assert_series_equal as real_assert_series_equal
 
 
-class Wrapper:
-    def __init__(self, df, cmp):
-        self.df = df
-        self.cmp = cmp
+def make_assert_equals(data_type, assert_equals, repr_function):
 
-    def __repr__(self):
-        return f"DataFrame({self.df.to_dict()!r})"
+    class Wrapper:
+        def __init__(self, df, cmp):
+            self.df = df
+            self.cmp = cmp
 
-    def __eq__(self, other):
-        if not isinstance(other, DataFrame):
-            return NotImplemented
-        return self.cmp(self.df, other)
+        def __repr__(self):
+            return f"{data_type.__name__}({repr(repr_function(self.df))})"
+
+        def __eq__(self, other):
+            if not isinstance(other, data_type):
+                return NotImplemented
+            return self.cmp(self.df, other)
+
+    @wraps(assert_equals)
+    def result(df, df_snapshot, *args, **kargs):
+        error: Optional[AssertionError] = None
+
+        def cmp(a, b):
+            nonlocal error
+            try:
+                assert_equals(a, b, *args, **kargs)
+            except AssertionError as e:
+                error = e
+                return False
+            return True
+
+        if not Wrapper(df, cmp) == df_snapshot:
+            assert error is not None
+            raise error
+
+    return result
 
 
-@wraps(real_assert_frame_equal)
-def assert_frame_equal(df, df_snapshot, *args, **kargs):
-    error: Optional[AssertionError] = None
-
-    def cmp(a, b):
-        nonlocal error
-        try:
-            real_assert_frame_equal(a, b, *args, **kargs)
-        except AssertionError as e:
-            error = e
-            return False
-        return True
-
-    if not Wrapper(df, cmp) == df_snapshot:
-        assert error is not None
-        raise error
+assert_frame_equal = make_assert_equals(
+    DataFrame, real_assert_frame_equal, lambda df: df.to_dict("records")
+)
+assert_series_equal = make_assert_equals(
+    Series, real_assert_series_equal, lambda df: df.to_dict()
+)
+assert_index_equal = make_assert_equals(
+    Index, real_assert_index_equal, lambda df: df.to_list()
+)
---
+++ tests/test_pandas.py
@@ -0,0 +1,42 @@
+import sys
+
+import pytest
+from pandas import DataFrame
+from pandas import Index
+from pandas import Series
+
+from inline_snapshot import snapshot
+from inline_snapshot._pandas import assert_frame_equal
+from inline_snapshot._pandas import assert_index_equal
+from inline_snapshot._pandas import assert_series_equal
+
+nan = float("nan")
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="no pandas for 3.9")
+def test_df():
+    df = DataFrame({"col0": [1, 2], "col1": [1, 5j], "col3": ["a", "b"]})
+
+    # the second argument can be a snapshot
+    assert_frame_equal(
+        df,
+        snapshot(
+            DataFrame(
+                [
+                    {"col0": 1, "col1": (1 + 0j), "col3": "a"},
+                    {"col0": 2, "col1": 5j, "col3": "b"},
+                ]
+            )
+        ),
+    )
+
+    # and can also be used without a snapshot
+    assert_frame_equal(df, df)
+
+    # for Index
+    index = Index(range(5))
+    assert_index_equal(index, snapshot(Index([0, 1, 2, 3, 4])))
+
+    # for Series
+    index = Series({1: 8, 5: 4})
+    assert_series_equal(index, snapshot(Series({1: 8, 5: 4})))
