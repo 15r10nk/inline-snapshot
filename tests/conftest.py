e4987677562254453b734a58bae78f6987944cb6
Frank Hoffmann <15r10nk-git@polarbit.de>
2024-12-15 20:32:33 +0100

    feat: disable inline-snapshot in CI runs

--- tests/conftest.py
+++ tests/conftest.py
@@ -344,6 +344,8 @@ def set_time(freezer):
             if "CI" in os.environ:
                 del os.environ["CI"]  # pragma: no cover
 
+            os.environ.pop("GITHUB_ACTIONS", None)
+
             try:
                 with mock.patch.dict(
                     os.environ,
