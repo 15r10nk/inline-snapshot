aead3c0b1e9266b5761912f7a2e09b8b3fcd9022
Frank Hoffmann <15r10nk-git@polarbit.de>
2025-01-09 07:16:59 +0100

    fix: load default config values even if `[tool.inline-snapshot]` is missing

--- src/inline_snapshot/_config.py
+++ src/inline_snapshot/_config.py
@@ -27,35 +27,35 @@ config = Config()
 
 def read_config(path: Path) -> Config:
     result = Config()
+    config = {}
     if path.exists():
-
         data = loads(path.read_text("utf-8"))
 
         try:
             config = data["tool"]["inline-snapshot"]
         except KeyError:
             pass
-        else:
-            try:
-                result.hash_length = config["hash-length"]
-            except KeyError:
-                pass
 
-            try:
-                result.default_flags = config["default-flags"]
-            except KeyError:
-                pass
+    try:
+        result.hash_length = config["hash-length"]
+    except KeyError:
+        pass
 
-            result.shortcuts = config.get(
-                "shortcuts", {"fix": ["create", "fix"], "review": ["review"]}
-            )
+    try:
+        result.default_flags = config["default-flags"]
+    except KeyError:
+        pass
 
-            if storage_dir := config.get("storage-dir"):
-                storage_dir = Path(storage_dir)
-                if not storage_dir.is_absolute():
-                    # Make it relative to pyproject.toml, and absolute.
-                    storage_dir = path.parent.joinpath(storage_dir).absolute()
-                result.storage_dir = storage_dir
+    result.shortcuts = config.get(
+        "shortcuts", {"fix": ["create", "fix"], "review": ["review"]}
+    )
+
+    if storage_dir := config.get("storage-dir"):
+        storage_dir = Path(storage_dir)
+        if not storage_dir.is_absolute():
+            # Make it relative to pyproject.toml, and absolute.
+            storage_dir = path.parent.joinpath(storage_dir).absolute()
+        result.storage_dir = storage_dir
 
     env_var = "INLINE_SNAPSHOT_DEFAULT_FLAGS"
     if env_var in os.environ:
