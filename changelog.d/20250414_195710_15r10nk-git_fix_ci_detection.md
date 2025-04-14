2e54f31b00077c5b43cf712ec3286a128ca7553e
Frank Hoffmann <15r10nk-git@polarbit.de>
2025-04-14 19:59:38 +0200

    fix: a pycharm environment is no longer incorrectly recognized as a ci environment

---
+++ changelog.d/20250414_195710_15r10nk-git_fix_ci_detection.md
@@ -0,0 +1,3 @@
+### Fixed
+
+- A pycharm environment is no longer incorrectly recognized as a CI environment.
--- tests/test_ci.py
+++ tests/test_ci.py
@@ -2,15 +2,17 @@ from inline_snapshot import snapshot
 from inline_snapshot.testing._example import Example
 
 
-def test_ci():
-    Example(
+def test_ci_run():
+    e = Example(
         """
 from inline_snapshot import snapshot
 def test_something():
     assert type(snapshot(5)) is int
 
     """
-    ).run_pytest(
+    )
+
+    e.run_pytest(
         env={"CI": "true"},
         report=snapshot(
             """\
@@ -18,8 +20,23 @@ INFO: CI run was detected because environment variable "CI" was defined.
 INFO: inline-snapshot runs with --inline-snapshot=disabled by default in CI.\
 """
         ),
-    ).run_pytest(
+    )
+
+    e.run_pytest(
         ["--inline-snapshot=disable"],
         env={"CI": "true"},
         report=snapshot(""),
+    )
+
+
+def test_no_ci_run():
+    Example(
+        """
+from inline_snapshot import snapshot
+def test_something():
+    assert not isinstance(snapshot(5),int)
+
+    """
+    ).run_pytest(
+        env={"TEAMCITY_VERSION": "true", "PYCHARM_HOSTED": "true"},
     )
