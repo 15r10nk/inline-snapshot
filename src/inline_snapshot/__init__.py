0765091e884c76214341af07eccadbfc8650eeb0
Frank Hoffmann <15r10nk-git@polarbit.de>
2025-05-06 08:37:32 +0200

    fix: moved formats into _format.py

--- src/inline_snapshot/__init__.py
+++ src/inline_snapshot/__init__.py
@@ -3,7 +3,7 @@ from ._code_repr import customize_repr
 from ._exceptions import UsageError
 from ._external import external
 from ._external._external import outsource
-from ._external._external import txt_like_suffix
+from ._external._format import txt_like_suffix
 from ._inline_snapshot import snapshot
 from ._is import Is
 from ._types import Category
