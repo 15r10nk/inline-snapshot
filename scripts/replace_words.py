707b0d59cd5e06c93d9b93fdbf81266ab49f7c8c
Frank Hoffmann <15r10nk-git@polarbit.de>
2025-07-31 09:20:52 +0200

    chore: added issue links to changelog

--- scripts/replace_words.py
+++ scripts/replace_words.py
@@ -4,13 +4,21 @@ import sys
 
 def replace_words(file_path, replacements):
     with open(file_path) as file:
-        content = file.read()
+        content = original_content = file.read()
 
     for old_word, new_word in replacements.items():
         content = re.sub(rf"\b{re.escape(old_word)}\b", new_word, content)
 
-    with open(file_path, "w") as file:
-        file.write(content)
+    content = re.sub(
+        rf"\(#([0-9]+)\)",
+        lambda m: f"([#{m[1]}](https://github.com/15r10nk/inline-snapshot/issues/{m[1]}))",
+        content,
+    )
+
+    if original_content != content:
+        with open(file_path, "w") as file:
+            print("change:", file_path)
+            file.write(content)
 
 
 if __name__ == "__main__":
@@ -20,5 +28,4 @@ if __name__ == "__main__":
     }
 
     for file_path in sys.argv[1:]:
-        print(file_path)
         replace_words(file_path, replacements)
