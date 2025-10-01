6f40d1a0958240f1e0d45e35d2f8ac5f1b32cc33
Frank Hoffmann <15r10nk-git@polarbit.de>
2025-10-01 12:32:31 +0200

    refactor: wip

--- src/inline_snapshot/_customize.py
+++ src/inline_snapshot/_customize.py
@@ -1,16 +1,35 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Callable
+
 custom_functions = []
 
+from dataclasses import MISSING
+from dataclasses import fields
+from dataclasses import is_dataclass
 
-def customize(f):
+
+def customize(f: Callable[[Any, Builder], Custom | None]):
     custom_functions.append(f)
     return f
 
 
 class Custom:
-    pass
+    def map(self, f):
+        raise NotImplementedError()
 
 
-class Default:
+def get_handler(v, builder: Builder) -> Custom:
+    for f in reversed(custom_functions):
+        r = f(v, builder)
+        if isinstance(r, Custom):
+            return r
+
+    return CustomValue(v)
+
+
+class CustomDefault(Custom):
     def __init__(self, value):
         self.value = value
 
@@ -64,8 +83,237 @@ class CustomCall(Custom):
         )
 
 
-def get_handler(v):
-    for f in reversed(custom_functions):
-        r = f(v)
-        if isinstance(r, Custom):
-            return r
+class CustomList(Custom):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def map(self, f):
+        return [f(x) for x in self.value]
+
+
+class CustomTuple(Custom):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def map(self, f):
+        return tuple([f(x) for x in self.value])
+
+
+class CustomDict(Custom):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def map(self, f):
+        return {f(k): f(v) for k, v in self.value}
+
+
+class CustomValue(Custom):
+    def __init__(self, value, repr_str=None):
+
+        if repr_str is None:
+            self.repr_str = repr_str
+        else:
+            self.repr_str = repr(value)
+
+        self.value = value
+
+    def map(self, f):
+        return self.value
+
+
+@customize
+def standard_handler(value, builder: Builder):
+    if isinstance(value, list):
+        return builder.List(value)
+
+    if isinstance(value, tuple):
+        return builder.Tuple(value)
+
+    if isinstance(value, dict):
+        return builder.Dict(value)
+
+
+@customize
+def dataclass_handler(value, builder: Builder):
+
+    if is_dataclass(value):
+
+        kwargs = {}
+
+        for field in fields(value):  # type: ignore
+            if field.repr:
+                field_value = getattr(value, field.name)
+                is_default = False
+
+                if field.default != MISSING and field.default == field_value:
+                    is_default = True
+
+                if (
+                    field.default_factory != MISSING
+                    and field.default_factory() == field_value
+                ):
+                    is_default = True
+
+                if is_default:
+                    field_value = builder.Default(field_value)
+                kwargs[field.name] = field_value
+
+        return builder.Call(type(value), [], kwargs)
+
+
+try:
+    import attrs
+except ImportError:  # pragma: no cover
+    pass
+else:
+
+    @customize
+    def attrs_handler(value, builder: Builder):
+
+        if attrs.has(value):
+
+            kwargs = {}
+
+            for field in attrs.fields(type(value)):
+                if field.repr:
+                    field_value = getattr(value, field.name)
+                    is_default = False
+
+                    if field.default is not attrs.NOTHING:
+
+                        default_value = (
+                            field.default
+                            if not isinstance(field.default, attrs.Factory)
+                            else (
+                                field.default.factory()
+                                if not field.default.takes_self
+                                else field.default.factory(value)
+                            )
+                        )
+
+                        if default_value == field_value:
+                            is_default = True
+
+                    if is_default:
+                        field_value = builder.Default(field_value)
+
+                    kwargs[field.name] = field_value
+
+            return builder.Call(type(value), [], kwargs)
+
+
+try:
+    import pydantic
+except ImportError:  # pragma: no cover
+    pass
+else:
+    # import pydantic
+    if pydantic.version.VERSION.startswith("1."):
+        # pydantic v1
+        from pydantic.fields import Undefined as PydanticUndefined  # type: ignore[attr-defined,no-redef]
+
+        def get_fields(value):
+            return value.__fields__
+
+    else:
+        # pydantic v2
+        from pydantic_core import PydanticUndefined
+
+        def get_fields(value):
+            return type(value).model_fields
+
+    from pydantic import BaseModel
+
+    @customize
+    def attrs_handler(value, builder: Builder):
+
+        if issubclass(value, BaseModel):
+
+            kwargs = {}
+
+            for name, field in get_fields(value).items():  # type: ignore
+                if getattr(field, "repr", True):
+                    field_value = getattr(value, name)
+                    is_default = False
+
+                    if (
+                        field.default is not PydanticUndefined
+                        and field.default == field_value
+                    ):
+                        is_default = True
+
+                    if (
+                        field.default_factory is not None
+                        and field.default_factory() == field_value
+                    ):
+                        is_default = True
+
+                    if is_default:
+                        field_value = builder.Default(field_value)
+
+                    kwargs[name] = field_value
+
+            return builder.Call(type(value), [], kwargs)
+
+
+@customize
+def namedtuple_handler(value, builder: Builder):
+    t = type(value)
+    b = t.__bases__
+    if len(b) != 1 or b[0] != tuple:
+        return
+    f = getattr(t, "_fields", None)
+    if not isinstance(f, tuple):
+        return
+    if not all(type(n) == str for n in f):
+        return
+
+    # TODO handle with builder.Default
+
+    return builder.Call(
+        type(value),
+        [],
+        {
+            field: getattr(value, field)
+            for field in value._fields
+            if field not in value._field_defaults
+            or getattr(value, field) != value._field_defaults[field]
+        },
+    )
+
+
+@customize
+def defaultdict_handler(value, builder: Builder):
+    if issubclass(value, defaultdict):
+        return CustomCall(
+            type(value),
+            [value.default_factory, dict(value)],
+        )
+
+
+class Builder:
+    def List(self, value) -> CustomList:
+        value = [get_handler(v, self) for v in value]
+        return CustomList(value)
+
+    def Tuple(self, value) -> CustomTuple:
+        value = tuple([get_handler(v, self) for v in value])
+        return CustomTuple(value)
+
+    def Call(self, function, posonly_args, kwargs, kwonly_args) -> CustomCall:
+        function = get_handler(function, self)
+        posonly_args = [get_handler(arg, self) for arg in posonly_args]
+        kwargs = {k: get_handler(arg, self) for k, arg in kwargs.items()}
+        kwonly_args = {k: get_handler(arg, self) for k, arg in kwonly_args.items()}
+
+        return CustomCall(function, *posonly_args, **kwargs).kwonly(**kwonly_args)
+
+    def Default(self, value) -> CustomDefault:
+        return CustomDefault(get_handler(value, self))
+
+    def Dict(self, value) -> CustomDict:
+        value = {get_handler(k, self): get_handler(v, self) for k, v in value.items()}
+        return CustomDict(value)
+
+    def Value(self, value, repr) -> CustomValue:
+        return CustomValue(value, repr)
