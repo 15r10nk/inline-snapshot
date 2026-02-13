677b49e667956a709dedbdf010a53365ff182ba9
Frank Hoffmann <15r10nk-git@polarbit.de>
2026-02-13 20:56:51 +0100

    Removed (customize preview) with mike 2.1.3

--- (customize preview)/plugins/replace_url.py
+++
@@ -1,6 +0,0 @@
-import mkdocs
-
-
-class ReplaceUrlPlugin(mkdocs.plugins.BasePlugin):
-    def on_page_content(self, html, page, config, files):
-        return html.replace("docs/assets", "assets")
